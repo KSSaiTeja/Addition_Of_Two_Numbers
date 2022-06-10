6b8a0f7f8da681ca940f0d304438cc8153871b36
KSSaiTeja <107067579+KSSaiTeja@users.noreply.github.com>
2022-06-10 07:37:56 +0530

    adding three numbers

--- Code.java
+++ Code.java
@@ -4,24 +4,26 @@ import java.util.*;
 
 public class Cons_examples {
 
-	Cons_examples(int x, int y) {
-		System.out.println("the result is: " + (x + y));
+	Cons_examples(int x, int y, int z) {
+		System.out.println("the result is: " + (x + y + z));
 	}
 
 	static void show() {
 		Scanner sc = new Scanner(System.in);
 		int x;
 		int y;
+		int z;
 		x = sc.nextInt();
 		y = sc.nextInt();
+		z = sc.nextInt();
 		@SuppressWarnings("unused")
-		Cons_examples obj = new Cons_examples(x, y);
+		Cons_examples obj = new Cons_examples(x, y, z);
 		sc.close();
 
 	}
 
 	public static void main(String[] args) {
-		System.out.println("enter the two numbers: ");
+		System.out.println("enter the three numbers: ");
 
 		Cons_examples.show();
 
